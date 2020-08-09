e33d6f6afaeee44e0e5837c562ac7cb6b619fe25
Marius Kroh <mail@mariuskroh.de>
2020-08-09 17:41:43 +0200

    :lipstick: Add base styles

---
+++ 03-fcc-frontend-libraries/03-fcc-drummachine/src/components/drummachine/DrumMachineStyles.js
@@ -0,0 +1,16 @@
+import { createUseStyles } from "react-jss";
+
+const useStyles = createUseStyles({
+  root: {
+    height: "100vh",
+  },
+  padsContainer: {
+    width: "50%",
+    height: "70vh",
+    display: "flex",
+    flexWrap: "wrap",
+    margin: "0 auto",
+  },
+});
+
+export default useStyles;
---
+++ 03-fcc-frontend-libraries/03-fcc-drummachine/src/components/drumpad/DrumPadStyles.js
@@ -0,0 +1,13 @@
+import { createUseStyles } from "react-jss";
+
+const useStyles = createUseStyles({
+  root: {
+    backgroundColor: "#444444",
+    width: "30%",
+    height: "30%",
+    borderRadius: "10px",
+    margin: "5px",
+  },
+});
+
+export default useStyles;
