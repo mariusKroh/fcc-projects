b8b9c29292e7d4df940b6f2bc140fbafea21ded5
Marius Kroh <mail@mariuskroh.de>
2020-08-13 14:32:03 +0200

    :zap: Add custom switch to toggle theme

--- 03-fcc-frontend-libraries/03-fcc-drummachine/src/components/switch/Switch.js
+++ 03-fcc-frontend-libraries/03-fcc-drummachine/src/components/switch/Switch.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ModelContext } from "../../context/ModelContext";
+import useStyles from "./SwitchStyles.js";
 
 const Switch = ({ onChange }) => {
+  // get styles
+  const classes = useStyles();
+  const { root, checkbox, slider } = classes;
+  // get context
+  const { is808 } = useContext(ModelContext);
+
   return (
-    <label style={{ position: "absolute", left: "20%", top: "500px" }}>
-      <input type="checkbox" onChange={onChange} />
-      <span></span>
+    <label className={root}>
+      <input type="checkbox" className={checkbox} onChange={onChange} />
+      <div className={slider}></div>
     </label>
   );
 };
