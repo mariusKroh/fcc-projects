ac8f5cbdaf6611e8ee5a5d54f78d946f0a888b62
Marius Kroh <mail@mariuskroh.de>
2020-08-13 16:48:30 +0200

    :zap: Add context to Display component

--- 03-fcc-frontend-libraries/03-fcc-drummachine/src/components/display/Display.js
+++ 03-fcc-frontend-libraries/03-fcc-drummachine/src/components/display/Display.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ModelContext } from "../../context/ModelContext";
+
 import useStyles from "./DisplayStyles.js";
 
-const Display = ({ text }) => {
+const Display = ({ text, isPlaying }) => {
   // get styles
   const classes = useStyles();
-  const { root, displayText } = classes;
+  const { root, model808, model909, display, displayText, active } = classes;
+  // get context
+  const { is808, toggle808 } = useContext(ModelContext);
   return (
-    <div id="display" className={root}>
-      <p className={displayText}>{text}</p>
+    <div id="display" className={`${root} ${is808 ? model808 : model909} `}>
+      <div className={`${display} ${isPlaying ? active : null} `}>
+        <p className={displayText}>{text}</p>
+      </div>
     </div>
   );
 };
