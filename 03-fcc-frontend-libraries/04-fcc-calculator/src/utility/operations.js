1385eb500f32a28db1129ff08de9598a56d86911
Marius Kroh <mail@mariuskroh.de>
2020-08-19 21:37:12 +0200

    :zap: Parse values as float to fix decimal issues

--- 03-fcc-frontend-libraries/04-fcc-calculator/src/utility/operations.js
+++ 03-fcc-frontend-libraries/04-fcc-calculator/src/utility/operations.js
@@ -1,4 +1,4 @@
-export const add = (a, b) => a + b;
-export const subtract = (a, b) => a - b;
-export const multiply = (a, b) => a * b;
-export const divide = (a, b) => a / b;
+export const add = (a, b) => parseFloat(a) + parseFloat(b);
+export const subtract = (a, b) => parseFloat(a) - parseFloat(b);
+export const multiply = (a, b) => parseFloat(a) * parseFloat(b);
+export const divide = (a, b) => parseFloat(a) / parseFloat(b);
