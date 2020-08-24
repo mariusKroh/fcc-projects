068f8ae67cafc4f669343bc69b466a26566e7e1c
Marius Kroh <mail@mariuskroh.de>
2020-08-24 08:41:42 +0200

    :zap: Add reset-css and set BaseStyles for App.js

--- 03-fcc-frontend-libraries/04-fcc-calculator/src/App.js
+++ 03-fcc-frontend-libraries/04-fcc-calculator/src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { useFccTest, FccTests } from "@asteffey/react-fcc-test";
 import Calculator from "./components/Calculator";
+import styles from "./BaseStyles";
 
 const App = () => {
   useFccTest({
     fccTest: FccTests.javascript_calculator,
   });
-  return <Calculator />;
+
+  const classes = styles();
+  const { root } = classes;
+
+  return (
+    <div className={root}>
+      <Calculator />
+    </div>
+  );
 };
 
 export default App;
---
+++ 03-fcc-frontend-libraries/04-fcc-calculator/src/BaseStyles.js
@@ -0,0 +1,14 @@
+import { createUseStyles } from "react-jss";
+
+const styles = createUseStyles({
+  root: {
+    width: "100vw",
+    height: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f1a8d3",
+  },
+});
+
+export default styles;
