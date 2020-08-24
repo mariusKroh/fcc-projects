8e76dda04261d1d8a8b649719140fd72992c9167
Marius Kroh <mail@mariuskroh.de>
2020-08-24 12:18:35 +0200

    :lipstick: Adjust display styles

--- 03-fcc-frontend-libraries/04-fcc-calculator/src/components/DisplayStyles.js
+++ 03-fcc-frontend-libraries/04-fcc-calculator/src/components/DisplayStyles.js
@@ -4,9 +4,10 @@ const styles = createUseStyles({
   root: {
     height: "100%",
     backgroundColor: "#a5dc4b",
+    fontFamily: "digital-7_monomono",
     borderRadius: "3px",
     boxShadow: "inset 1px 0px 3px 3px #79a734",
-    fontSize: "3.5rem",
+    fontSize: "5rem",
     display: "flex",
     alignItems: "center",
     textShadow: "2px 1px #79a734",
